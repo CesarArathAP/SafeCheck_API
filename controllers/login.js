efd5e13e5c27f65fc845218c9a06a6a4d9bfe5aa
CesarArathAP <1722110585@utectulancingo.edu.mx>
2024-03-21 12:35:30 -0600

    ya quedo

--- controllers/login.js
+++ controllers/login.js
@@ -6,14 +6,8 @@ const login = async (req, res) => {
   const { username, password } = req.body;
 
   try {
-    // Registro de consola para verificar el nombre de usuario
-    console.log('Username:', username);
-
     // Buscar el docente por nombre de usuario
     const docente = await Docente.findOne({ username });
-
-    // Registro de consola para verificar si se encuentra el docente
-    console.log('Docente encontrado:', docente);
 
     if (!docente) {
       return res.status(404).json({ error: 'Usuario no encontrado' });
@@ -24,8 +18,14 @@ const login = async (req, res) => {
       return res.status(401).json({ error: 'Contraseña incorrecta' });
     }
 
-    // Autenticación exitosa
-    res.status(200).json({ message: `¡Bienvenido ${docente.nombre}!` });
+    // Construir el mensaje de bienvenida con el nombre del docente y sus carreras asignadas
+    const nombreCompleto = `${docente.nombre} ${docente.apellido_paterno} ${docente.apellido_materno}`;
+    const carreras = docente.carreras.map(carrera => carrera.nombre).join(', ');
+
+    const mensajeBienvenida = `¡Bienvenido ${nombreCompleto}! Sus carreras asignadas son: ${carreras}`;
+
+    // Autenticación exitosa, devolver solo el mensaje de bienvenida
+    res.status(200).json({ message: mensajeBienvenida });
   } catch (error) {
     console.error('Error de autenticación:', error);
     res.status(500).json({ error: 'Error de servidor' });
