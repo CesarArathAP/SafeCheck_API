362bc240bd872fe705737cec13d0b142f45b92fa
CesarArathAP <1722110585@utectulancingo.edu.mx>
2024-03-28 17:05:21 -0600

    XD

--- controllers/registrarVisitas.js
+++ controllers/registrarVisitas.js
@@ -1,15 +1,27 @@
 // controllers/registrarVisitas.js
 
 const Visitas = require('../models/Visitas');
+const fs = require('fs').promises;
+const path = require('path');
 
 async function registrarVisita(req, res, next) {
   try {
     const visitaData = req.body;
-    // Capturar la ruta del archivo cargado
-    const fotografia = req.file.path; // Esto asume que multer ha guardado la imagen en el sistema de archivos y ha adjuntado la ruta al objeto req.file
+    // Leer la imagen cargada y convertirla a base64
+    const fotografia = req.file.path;
+    const tipoMIME = req.file.mimetype;
+    const imagenBuffer = await fs.readFile(fotografia);
+    const imagenBase64 = `data:${tipoMIME};base64,${imagenBuffer.toString('base64')}`;
 
-    // Agregar la ruta de la fotografía a los datos de la visita
-    visitaData.visita.fotografia = fotografia;
+    // Agregar la imagen base64 a los datos de la visita
+    visitaData.visita.fotografia = imagenBase64;
+
+    // Obtener el último ID y sumarle 1 para generar un nuevo ID
+    const ultimoId = await Visitas.findOne({}, {}, { sort: { 'id': -1 } });
+    const nuevoId = ultimoId ? ultimoId.id + 1 : 1;
+
+    // Agregar el ID al objeto visitaData
+    visitaData.id = nuevoId;
 
     const nuevaVisita = new Visitas(visitaData);
     await nuevaVisita.save();
